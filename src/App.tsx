f23334f4453bfa99bc6ba9decbdc9d31ad422d78
Andeilson Ferreira <agf@MacBook-Pro-de-Andeilson.local>
2022-06-26 20:36:49 -0300

    Project Started

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {
+  Container
+} from './styles';
+
+import GlobalStyles from './styles/global';
+
+export function App() {
+  return (
+    <>
+    <GlobalStyles />
+      <div></div>
+    </>
+    
+  );
+}
